3a8cd00e614bac2d1fc3a699dd500c1c7799c04f
Roman Glogowski <18666546+diablo39@users.noreply.github.com>
2018-07-31 21:20:45 +0200

    removing old code

--- src/AspNetCoreExample/Program.cs
+++ src/AspNetCoreExample/Program.cs
@@ -15,32 +15,7 @@ namespace AspNetCoreExample
     {
         public static void Main(string[] args)
         {
-            bool appNamePrinted = PrintAppName(args);
-
-            if (appNamePrinted == false)
-            {
-                CreateWebHostBuilder(args).Build().Run();
-            }
-        }
-
-        private static bool PrintAppName(string[] args)
-        {
-            var commandHandled = false;
-            var app = new Microsoft.Extensions.CommandLineUtils.CommandLineApplication();
-            app.Command("appName", config =>
-            {
-                config.OnExecute(() =>
-                {
-                    var currentAssembly = Assembly.GetExecutingAssembly();
-                    var product = currentAssembly.GetCustomAttribute<AssemblyProductAttribute>();
-                    Console.WriteLine(product.Product);
-                    commandHandled = true;
-                    return 0;
-                });
-            });
-
-            app.Execute(args);
-            return commandHandled;
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
